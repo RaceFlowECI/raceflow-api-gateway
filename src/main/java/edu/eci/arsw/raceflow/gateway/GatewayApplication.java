053ef5e0e5680ca84b57dc59686cdd44d527c3cd
Juan Sebastian <juan.guayazan-c@mail.escuelaing.edu.co>
2026-07-03 20:00:00 -0500

    feat: initialize Spring Boot 3.2 project structure

---
+++ src/main/java/edu/eci/arsw/raceflow/gateway/GatewayApplication.java
@@ -0,0 +1,11 @@
+package edu.eci.arsw.raceflow.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
---
+++ src/test/java/edu/eci/arsw/raceflow/gateway/GatewayApplicationTests.java
@@ -0,0 +1,10 @@
+package edu.eci.arsw.raceflow.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+    @Test
+    void contextLoads() {}
+}
